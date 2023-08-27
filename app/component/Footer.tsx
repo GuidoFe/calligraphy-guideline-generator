ca2d6ec81f457ff1024bcc5f246a74781f0ae6df
guidofe <guido.ferri@protonmail.com>
2023-08-27 15:38:16 +0200

    Updated footer

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,16 +1,15 @@
 import { TbBrandGithub, TbHeart, TbMail } from "react-icons/tb";
+import { SiKofi} from 'react-icons/si'
+import Image from 'next/image';
 
 export function Footer() {
   return (
     <footer className="Footer">
       <p className="is-size-7">
-      Created with <TbHeart /> by Guido Ferri. <a 
-        href="https://github.com/GuidoFe/calligraphy-guideline-generator"
-        target="_blank">
-          <TbBrandGithub /> Repo
-        </a> <a href="mailto:guido.ferri@protonmail.com">
-          <TbMail /> Mail
-        </a>
+      Created with <TbHeart /> by Guido Ferri. <TbBrandGithub /> <a 
+          href="https://github.com/GuidoFe/calligraphy-guideline-generator"
+          target="_blank">Repo</a> <TbMail /> <a href="mailto:guido.ferri@protonmail.com">
+          Mail</a> <SiKofi /> <a href="https://ko-fi.com/Y8Y367Y7T" target="_blank">Support me on Ko-fi</a>
       </p>
     </footer>
   )
